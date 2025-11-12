270c2a780b68572716dd64e09fc2a8127b08890f
southernimp <eziochiuchina@hotmail.com>
2025-11-12 14:15:07 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
 mkdir -p build
-cmake -G Ninja -S . -B build \
-      -DCMAKE_C_COMPILER="gcc" \
-      -DCMAKE_CXX_COMPILER="g++" \
-      -DCMAKE_INSTALL_PREFIX="install" \
-      -DCMAKE_BUILD_TYPE=Release
-ninja -j`nproc` -C build install
+
+cd build
+
+cmake .. \
+    -DCMAKE_SYSTEM_NAME=Windows \
+    -DCMAKE_C_COMPILER=x86_64-w64-mingw32-gcc \
+    -DCMAKE_CXX_COMPILER=x86_64-w64-mingw32-g++ \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_ENABLE_ASSERTIONS=OFF \
+    -DLLVM_INCLUDE_BENCHMARKS=OFF \
+    -DLLVM_INCLUDE_TESTS=OFF \
+    -DLLVM_INCLUDE_EXAMPLES=OFF \
+    -DLLVM_ENABLE_PROJECTS="clang" \
+    -DLLVM_TARGETS_TO_BUILD="ARM" \
+    -G Ninja
+ninja
